779a309d87cb2b12a15c8991b709ff3b3e47e2ab
imanadetunji <theiman.adetunji@gmail.com>
2023-07-19 16:02:41 -0400

    added properties to application.properties

--- src/test/java/com/example/entitiesexercise1/EntitiesExercise1ApplicationTests.java
+++ src/test/java/com/example/entitiesexercise1/EntitiesExercise1ApplicationTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -26,7 +25,6 @@ class EntitiesExercise1ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 
 	@Test
 	public void testPriceLessThan() {
